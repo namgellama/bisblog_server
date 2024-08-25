83c74cbbccbf27f70f7874038da222cd715b39f2
Namgel <namgeltamang14@gmail.com>
2024-08-25 17:49:10 +0545

    Implement post update

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -2,10 +2,15 @@ import { Request, Response } from "express";
 import asyncHandler from "../middlewares/asyncHandler";
 import prisma from "../prismaClient";
 
-interface PostRequestDTO {
+interface CreatePostRequestDTO {
 	title: string;
 	body: string;
 	userId: string;
+}
+
+interface UpdatePostRequestDTO {
+	title: string;
+	body: string;
 }
 
 const getAllPosts = asyncHandler(
@@ -24,7 +29,10 @@ const getAllPosts = asyncHandler(
 );
 
 const createPost = asyncHandler(
-	async (request: Request<{}, {}, PostRequestDTO>, response: Response) => {
+	async (
+		request: Request<{}, {}, CreatePostRequestDTO>,
+		response: Response
+	) => {
 		const { title, body } = request.body;
 
 		const newPost = await prisma.post.create({
@@ -39,4 +47,41 @@ const createPost = asyncHandler(
 	}
 );
 
-export { createPost, getAllPosts };
+const updatePost = asyncHandler(
+	async (
+		request: Request<{ id: string }, {}, UpdatePostRequestDTO>,
+		response: Response
+	) => {
+		const { title, body } = request.body;
+
+		const post = await prisma.post.findUnique({
+			where: {
+				id: request.params.id,
+			},
+		});
+
+		if (post) {
+			if (post.userId === request.user.id) {
+				const updatedPost = await prisma.post.update({
+					where: {
+						id: request.params.id,
+					},
+					data: {
+						title,
+						body,
+					},
+				});
+
+				response.status(200).json(updatedPost);
+			} else {
+				response.status(403);
+				throw new Error("Forbidden");
+			}
+		} else {
+			response.status(404);
+			throw new Error("Post not found.");
+		}
+	}
+);
+
+export { createPost, getAllPosts, updatePost };
--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,10 +1,15 @@
 import express from "express";
-import { createPost, getAllPosts } from "../controllers/postController";
+import {
+	createPost,
+	getAllPosts,
+	updatePost,
+} from "../controllers/postController";
 import { protect } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
 router.route("/").get(getAllPosts);
 router.route("/").post(protect, createPost);
+router.route("/:id").put(protect, updatePost);
 
 export default router;
